5f01ae91bda5f4894f59604e21f4da109a6a49d0
yakeenkapali <be2019se36@gces.edu.np>
2022-03-21 16:05:56 +0545

    Remaining List Added

--- Stack/pushAndPop.cpp
+++ Stack/pushAndPop.cpp
@@ -3,11 +3,12 @@ using namespace std;
 
 class Stack{
 	int top;
+	int count = 0;
 	int stack[10];
 	
 	public:
 		Stack(){
-			top == -1;
+			top = -1;
 		};
 	
 	bool isFull(){
@@ -34,6 +35,8 @@ class Stack{
 			stack[top] = data;
 			cout << data << " pushed in stack\n";		
 		}
+		
+		count++;
 	}
 	
 	int pop(){
@@ -47,6 +50,14 @@ class Stack{
 			top = top -1 ;	
 			return data;
 		}
+		
+		count--;
+	}
+	
+	void displayValue(){
+		for(int i=0; i<count; i++){
+			cout << stack[i] << " ";
+		}
 	}
 };
 
@@ -57,7 +68,9 @@ int main(){
 	s.push(20);
 	s.push(30);
 	
-	cout << s.pop() << " popped from stack";
+	cout << s.pop() << " popped from stack\n";
+	
+	cout << "Elements in the list: " ; s.displayValue();
 	 
 }
 
