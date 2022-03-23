1c921e11f0ecade99ce35d64992f551b183f31c7
yakeenkapali <be2019se36@gces.edu.np>
2022-03-23 17:50:14 +0545

    Insertion Sort Added

---
+++ Sorting/Insertion Sort/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#define MAX 4
+using namespace std;
+
+class InsertionSort{
+	
+	int data[MAX] = {4, 3, 2, 1};
+	int key = 0;	
+	
+	public:
+		void insertionSort(){
+			for(int i = 1; i<MAX; i++){
+				key = data[i];
+				int j = i-1;
+				
+				while(j>=0 && data[j] > key ){
+					data[j+1] = data[j];
+					j = j-1;
+				}
+				data[j+1] = key;
+			}
+		}
+		
+		void displayBeforeSort(){
+			for (int i = 0; i < MAX; i++) {
+  				cout << data[i] << " ";
+			}
+			cout << endl;
+		}
+		
+		void displayAfterSort(){
+			for(int i=0; i<MAX; i++){
+				cout << data[i] << " ";
+			}
+		}
+};
+
+int main(){
+	InsertionSort sort;
+	sort.displayBeforeSort();
+	sort.insertionSort();
+	sort.displayAfterSort();
+}
